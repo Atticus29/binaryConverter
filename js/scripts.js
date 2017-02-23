35b33c86a7fbfc1d2e6c3366ac7271ab30e69b06
Mark <mark.aaron.fisher@gmail.com>
2017-02-22 18:18:03 -0800

    add ability to use convertXnaryToInt for any base 1-10

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,11 @@ var validateXnary = function (str, base){
     for (var i=0; i<base; i++){
       validNumbersArray.push(i);
     }
-    // console.log(validNumbersArray);
     var counter = 0;
     for(var i = 0; i<str.length; i++){
-      // console.log("validNumbersArray is " + validNumbersArray);
-      // console.log("str[i] is " + str[i]);
+
       if(validNumbersArray.indexOf(parseInt(str[i]))>-1){
         counter ++;
-        // console.log("counter is now " + counter);
       }
     }
     if(str.length===counter){
@@ -26,9 +23,10 @@ var validateXnary = function (str, base){
 // console.log(validateXnary("12122", 3));
 // console.log(validateXnary("12322", 3));
 
-var convertXnaryToInt = function(str){
+var convertXnaryToInt = function(str, base){
   // var inputInt = parseInt(str);
   if(!parseInt(str)){
+    // currently also throws this if str is just "0"
     alert("Please enter a integer number");
     return parseInt(str);
   }
@@ -37,18 +35,24 @@ var convertXnaryToInt = function(str){
   for (var i=n-1; i>=0; i--){
     // console.log("i is " + i);
     // console.log("value in that slot is " + (Math.pow(2,n-i-1) * parseInt(str[i])));
-    answer += Math.pow(2,n-i-1) * parseInt(str[i]);
+    answer += Math.pow(base,n-i-1) * parseInt(str[i]);
   }
   // console.log("answer is " + answer);
   return answer;
 }
 
-// console.log(convertXnaryToInt("hi"));
-// console.log(convertXnaryToInt("0"));
-// console.log(convertXnaryToInt("1"));
-// console.log(convertXnaryToInt("10"));
-// console.log(convertXnaryToInt("11"));
-// console.log(convertXnaryToInt("100"));
+// console.log(convertXnaryToInt("hi",2));
+// console.log(convertXnaryToInt("0",2));
+// console.log(convertXnaryToInt("1",2));
+// console.log(convertXnaryToInt("10",2));
+// console.log(convertXnaryToInt("11",2));
+// console.log(convertXnaryToInt("100",2));
+// console.log(convertXnaryToInt("hi",3));
+// console.log(convertXnaryToInt("0",3));
+// console.log(convertXnaryToInt("1",3));
+// console.log(convertXnaryToInt("10",3));
+// console.log(convertXnaryToInt("11",3));
+// console.log(convertXnaryToInt("100",3));
 
 
 
